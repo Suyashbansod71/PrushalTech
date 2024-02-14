776081a939df16d94a5fc4b6f4e261f709b674c0
Suyash Bansod <77398556+Suyashbansod71@users.noreply.github.com>
2024-02-14 23:44:09 +0530

    Updated Navbar with Hamburger Menu And About Us Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,23 @@ import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
     const [menuOpen,setMenuOpen] = useState(false);
+
+   
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    };
+
+
   return (
     <>
-     <nav className='NavbarItems' >
+     {/* <nav className='NavbarItems' >
         <h1 className='logo'>Finsweet<i className='fab fa-react'></i></h1>
-        {/* <div className='menu-icons'>
+        <div className='menu-icons'>
             <i className='fa fa-bars'></i>
-        </div> */}
+        </div>
          <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
         <span></span>
         <span></span>
@@ -30,10 +40,47 @@ const Navbar = () => {
           <NavLink className="nav-link" to="/contact">Contact us</NavLink>
             </li> 
             <li>
-          <NavLink className="nav-link" to="/">Home</NavLink>
+          <NavLink className="nav-link" to="/">Search</NavLink>
             </li> 
         </ul>
-     </nav>
+     </nav> */}
+
+
+<nav>
+      <Link to="/" onClick={scrollToTop} className="title">
+        
+      <h3 className='logo'>Finsweet<i className='fab fa-react'></i></h3>
+      </Link>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="headerstruct d-sm-flex align-self-center">
+
+      
+      <ul className={menuOpen ? "open m-0 p-0" : "m-0 p-0"}>
+        <li>
+          <NavLink className="py-2 mx-4" to="/" onClick={scrollToTop}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink className="py-2 mx-4" to="/about" onClick={scrollToTop}>About Us</NavLink>
+        </li>
+        <li>
+          <NavLink className="py-2 mx-4" to="/services" onClick={scrollToTop}>What We Do</NavLink>
+        </li>
+        <li>
+          <NavLink className="py-2 mx-4" to="/contact" onClick={scrollToTop}>Media</NavLink>
+        </li>
+        <li>
+          <NavLink className="py-2 mx-4" to="/" onClick={scrollToTop}>Contact</NavLink>
+        </li>
+      </ul>
+      </div>
+    </nav>
+
+
+
     </>
   )
 }
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
-
+import './aboutStyle.css'
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <>
+     <div className='about'>
+      <div className='about-container'>
+      <h4>About Us</h4>
+      <hr/>
+      <h1> joinklas dasdam sdmansdnasda 
+      sdmansdnasda 
+      </h1>
+      <p>Have any question in mind or want to enquire? Please feel free to contact us through the form or the following details.</p>
+      </div>
+
+      <div className='about-container'>
+       <img src='img3.jpg' className = 'img1' />
+      </div>
+     </div>
+
+     <div className='about-para1'>
+      <div className='about-para1-container'>
+      <h3>lorem ipsssnn  asjdk    kansdnfa   alksndflksd</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus,
+         mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id 
+         rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+      </div>
+      <div className='about-para1-container'>
+      <p>Overview</p>
+      <hr/>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. 
+
+Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+     
+      </div>
+     </div>
+   <hr/>
+   <div className='about-para2'>
+    <div className='about-para2-container'>
+    <h3>How to join us on Prushal Tech</h3>
+    </div>
+    <div className='about-para2-container'>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.  Nunc ut sem vitae risus tristique posuere.</p>
+    </div>
+    <div className='about-para2-container'>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra </p>
+    </div>
+
+   </div>
+ 
+     <img src='img2.jpg' className = 'img2' width= '80%'/>
+
+    </>
   )
 }
 
